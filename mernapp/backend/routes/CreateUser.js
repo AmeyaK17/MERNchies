d2fd7b708e3544b9bb4aa2e24f800c9082b52b04
Ameya Kale <ameya.k.kale@gmail.com>
2023-08-30 19:34:36 -0400

    Added Signin/Login functionality

--- mernapp/backend/routes/CreateUser.js
+++ mernapp/backend/routes/CreateUser.js
@@ -29,4 +29,33 @@ router.post("/createuser", [
         }
 })
 
+router.post('/loginuser', [
+        body('email', 'Not a valid email').isEmail(),
+        body('password', 'Password too short').isLength({min: 5})
+    ],
+    async(req, res) => {
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()})
+        }
+        let email = req.body.email
+        try {
+            let userData = await User.findOne({email})
+            if(!userData){
+                return res.status(400).json({errors: 'Try logging with correct email id'})
+            }
+
+            if(req.body.password !== userData.password){
+                return res.status(400).json({errors: 'Wrong password'})
+            }
+            
+            return res.json({success: true})
+        }
+        catch(err){
+            console.log(err)
+            res.json({success: false})
+        }
+    }
+)
+
 module.exports = router
